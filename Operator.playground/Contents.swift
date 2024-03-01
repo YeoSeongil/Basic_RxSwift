cbb07679dacc487d357b53d0dfa393b560a50982
YeoSeongil <seongil_yeo@naver.com>
2024-03-01 15:57:18 +0900

    flatmap

--- Operator.playground/Contents.swift
+++ Operator.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
+import RxSwift
 
-var greeting = "Hello, playground"
+
+let disposedBag = DisposeBag()
+// startWith
+//Observable.from([1,2,3])
+//    .startWith(0)
+//    .startWith(-2,-1)
+//    .subscribe { print($0) }
+//    .disposed(by: disposedBag)
+
+// flatmap
+let someObservable1 = Observable.of(1, 2, 3)
+let someObservable2 = Observable.of("A", "B", "C")
+
+someObservable1
+    .flatMap { num -> Observable<String>  in
+        print("방출되는 Num item : \(num)")
+        return someObservable2
+    }
+    .subscribe { print($0)
+    }
