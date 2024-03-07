029ae50fabed244936d3400b1591e4dcd36d0b7d
YeoSeongil <seongil_yeo@naver.com>
2024-03-07 19:26:38 +0900

    DI, DIP, 의존성 공부

--- Dependency.playground/Contents.swift
+++ Dependency.playground/Contents.swift
@@ -1,3 +1,42 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+protocol EngineProtocol {
+    func start()
+}
+
+// 하위 모듈
+class Engine: EngineProtocol {
+    func start() {
+        print("엔진 가동")
+    }
+}
+
+class ElectricMotor: EngineProtocol {
+    func start() {
+        print("전기모터 가동")
+    }
+}
+
+// 상위 모듈
+class Car {
+    private let engine: EngineProtocol
+    
+    init(engine: EngineProtocol) {
+        self.engine = engine
+    }
+    
+    func start() {
+        engine.start()
+        print("차가 출발합니다.")
+    }
+}
+
+let engine = Engine()
+let myCar = Car(engine: engine)
+myCar.start()
+
+let electricMotor = ElectricMotor()
+let myCar2 = Car(engine: electricMotor)
+myCar2.start()
+
+
