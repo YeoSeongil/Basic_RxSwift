3be4d72ef81199d90dc1c02a1a8d9d3314b68397
YeoSeongil <seongil_yeo@naver.com>
2024-03-08 23:09:11 +0900

    combineLatest

--- CombineLatest.playground/Contents.swift
+++ CombineLatest.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
+import RxSwift
 
-var greeting = "Hello, playground"
+let disposeBag = DisposeBag()
+let left = PublishSubject<String>()
+let right = PublishSubject<String>()
+
+//let observable = Observable
+//    .combineLatest(left, right) { (lastLeft, lastRight) in
+//        "\(lastLeft) \(lastRight)"
+//    }
+
+
+let observable2 = Observable
+    .combineLatest(left, right)
+
+observable2.subscribe(onNext: { (left, right) in
+    print(left)
+    print(right)
+}).disposed(by: disposeBag)
+
+left.onNext("Hello")
+right.onNext("World")
+
+left.onNext("Hello2")
+right.onNext("World2")
+
+
